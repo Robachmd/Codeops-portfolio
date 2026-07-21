60eb09585ab0c50198f96eda103d384953e4ef9f
robachmd <roba.chimdi.fufa@gmail.com>
2026-07-21 23:23:36 +0300

    adding day07 project

--- IBT/Module_01/day07/account_registry/main.py
+++ IBT/Module_01/day07/account_registry/main.py
@@ -4,7 +4,7 @@ class Account:
         self.account_number = account_number
         self.__private_balance = private_balance
         self.observers = []
-        
+        self.history=[]
     @property
     def balance(self):
         return self.__private_balance 
@@ -18,21 +18,33 @@ class Account:
         self.__private_balance += amount
         message= f"this transaction happen from your account type : - {self.account_type}"        
         self.notify(message)
+        self.history.append(f"deposit,{amount}")
     def withdraw(self,amount):
         if amount > self.__private_balance :
             print("insuficient fund")
             return
         self.__private_balance -= amount
         self.notify(f"-you withdraw= {amount} now your balance is {self.balance}etb")
+        self.history.append(f"withdraw,{amount}")
+
     def statement(self,account_type="main"):
         self.account_type=account_type
         message= f"this transaction happen from your account type : - {self.account_type}"
         self.notify(message)
+        self.history.append(f"Account type,{self.account_type}")
+        return message
+
     def subscribe(self,obs):
         self.observers.append(obs)
     def notify(self,event):
         for obs in self.observers:
             obs.update(event)
+            
+    def undo_last(self):
+        return f"this is your last_transaction or undo {self.history.pop()}"
+            
+   
+
 class SMSAlert:
     def update(self,event):
         print(f"[Telebirr SMS]{event}")
@@ -79,17 +91,47 @@ class AccountFactor:
         if kind == "account":
                     return Account(owner,account_number,private_balance)                    
 
+class AccountRegistry:
+    def __init__(self):
+        self.accounts_data={}
+
+    def add_account(self, account):
+        self.accounts_data[account.account_number] = account
+
+    def find_account(self, account_number):
+        return self.accounts_data.get(account_number)
+    def list_all(self):
+        return sorted(self.accounts_data.keys())
+
 current_Account=AccountFactor.create("currentaccount","Roba", "ADB-10002938", 2000,20000)
 
-Saving_Account=AccountFactor.create("savingaccount","sami", "10007498", 5000,0.5)
-main_Account=AccountFactor.create("account","sami", "ADB-10007498", 800,0.5)
+Saving_Account=AccountFactor.create("savingaccount","sami", "ADB-10007498", 5000,0.5)
+main_Account=AccountFactor.create("account","sami", "ADB-10006749", 800,0.5)
 accounts=[main_Account,Saving_Account,current_Account]
 for i in accounts:
+    i.statement()
     i.subscribe(SMSAlert())
     i.withdraw(100)
-    i.statement()
+    i.account_number
 
     print(f"this is account balance after each class:{i.balance}")
 Saving_Account.add_intrest()
 
 print(f"this is intrest of saving account value = {Saving_Account.balance}")
+
+registry = AccountRegistry()
+registry.add_account(main_Account)
+registry.add_account(current_Account)
+registry.add_account(Saving_Account)
+findAccount=registry.find_account("ADB-10007498")
+findAccount.deposit(500)
+findAccount.withdraw(300)
+print(findAccount.statement())
+print(registry.list_all())
+
+print(findAccount.history)
+print(findAccount.undo_last())
+print(findAccount.owner)
+print(findAccount.balance)
+print(findAccount.owner)
+print(findAccount.account_type)
---
+++ IBT/Module_01/day07/account_registry/practice.py
@@ -0,0 +1,7 @@
+#  Operation                  | Big-O          | Why?                    
+# -----------------------------------------------------------------------------------------------                              
+#  List index                 | O(1)           | Go directly to the position.                          
+#  Single loop                | O(n)           | Visit each element once.                              
+#  Nested loop                | O(n²)          | For each element, visit every element again.          
+#  Dictionary lookup          | O(1)           | Use the key to find the value directly.              
+#  Binary Search              | O(log n        | Throw away half the remaining search space each step. 
